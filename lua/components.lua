9d86aa724ad7052afdf653fd81b1a25cda31a65b
matheus moreno <o.matheus.moreno@gmail.com>
2023-02-13 15:10:27 -0300

    just a commit, there is no really changes in this file

--- lua/components.lua
+++ lua/components.lua
@@ -1,92 +1,92 @@
 ---------------- Appearence Components ----------------
 
-require('interface')
+require("interface")
 
 local function components()
-	local painter = require('tools/painter')
+	local painter = require("tools/painter")
 
-	local back = eval['synIDattr'](eval['hlID']("Normal"), "bg")
-	local fore = eval['synIDattr'](eval['hlID']("Normal"), "fg")
+	local back = eval["synIDattr"](eval["hlID"]("Normal"), "bg")
+	local fore = eval["synIDattr"](eval["hlID"]("Normal"), "fg")
 
-	back = (back ~= '') and back or '#282828'
-	fore = (fore ~= '') and fore or '#d8d8d8'
+	back = (back ~= "") and back or "#282828"
+	fore = (fore ~= "") and fore or "#d8d8d8"
 
 	local pieces = {
 		colors = {
-			base = {back, back, back, back, back, back, back, back, back, back},
-			case = {fore, fore, fore, fore, fore, fore, fore, fore, fore, fore},
+			base = { back, back, back, back, back, back, back, back, back, back },
+			case = { fore, fore, fore, fore, fore, fore, fore, fore, fore, fore },
 			black = {
-				varglobal('terminal_color_0'),
-				varglobal('terminal_color_8')
+				varglobal("terminal_color_0"),
+				varglobal("terminal_color_8"),
 			},
 			red = {
-				varglobal('terminal_color_1'),
-				varglobal('terminal_color_9')
+				varglobal("terminal_color_1"),
+				varglobal("terminal_color_9"),
 			},
 			green = {
-				varglobal('terminal_color_2'),
-				varglobal('terminal_color_10')
+				varglobal("terminal_color_2"),
+				varglobal("terminal_color_10"),
 			},
 			yellow = {
-				varglobal('terminal_color_3'),
-				varglobal('terminal_color_11')
+				varglobal("terminal_color_3"),
+				varglobal("terminal_color_11"),
 			},
 			blue = {
-				varglobal('terminal_color_4'),
-				varglobal('terminal_color_12')
+				varglobal("terminal_color_4"),
+				varglobal("terminal_color_12"),
 			},
 			magenta = {
-				varglobal('terminal_color_5'),
-				varglobal('terminal_color_13')
+				varglobal("terminal_color_5"),
+				varglobal("terminal_color_13"),
 			},
 			cyan = {
-				varglobal('terminal_color_6'),
-				varglobal('terminal_color_14')
+				varglobal("terminal_color_6"),
+				varglobal("terminal_color_14"),
 			},
 			white = {
-				varglobal('terminal_color_7'),
-				varglobal('terminal_color_15')
-			}
+				varglobal("terminal_color_7"),
+				varglobal("terminal_color_15"),
+			},
 		},
 		separators = {
-			arrow = {'', ''},
-			round = {'', ''},
-			slant = {'◢', '◣'},
-			blank = {'▐', '▌'},
+			arrow = { "", "" },
+			round = { "", "" },
+			slant = { "◢", "◣" },
+			blank = { "▐", "▌" },
 		},
 		divisors = {
-			arrow = {'', ''},
-			round = {'', ''},
-			slant = {'╱', '╲'},
-			blank = {'🭳', '🭳'}, -- 🭳│┃
-			point = {'•', '•'}
+			arrow = { "", "" },
+			round = { "", "" },
+			slant = { "╱", "╲" },
+			blank = { "🭳", "🭳" }, -- 🭳│┃
+			point = { "•", "•" },
 		},
 		models = {
 			flow = {
-				{2, 2},
-				{2, 2}
+				{ 2, 2 },
+				{ 2, 2 },
 			},
 			mirror = {
-				{2, 2},
-				{1, 1}
+				{ 2, 2 },
+				{ 1, 1 },
 			},
 			block = {
-				{1, 2},
-				{1, 2}
-			}
-		}
+				{ 1, 2 },
+				{ 1, 2 },
+			},
+		},
 	}
 
 	for color, content in pairs(pieces.colors) do
 		if #content == 0 then
-			pieces.colors[color] = {back, fore}
+			pieces.colors[color] = { back, fore }
 		end
 	end
 
 	local level
 	local multiplier
 
-	if get('background') == 'dark' then
+	if get("background") == "dark" then
 		multiplier = -1
 	else
 		multiplier = 1
@@ -95,19 +95,19 @@ local function components()
 	level = 20 * multiplier
 
 	for index = 1, 10 do
-		local base = pieces.colors['base'][index]
-		local case = pieces.colors['case'][index]
+		local base = pieces.colors["base"][index]
+		local case = pieces.colors["case"][index]
 
-		base = painter.brightness(base,  level)
+		base = painter.brightness(base, level)
 		case = painter.brightness(case, -level)
 
-		pieces.colors['base'][index] = base
-		pieces.colors['case'][index] = case
+		pieces.colors["base"][index] = base
+		pieces.colors["case"][index] = case
 
 		level = level - 10 * multiplier
 	end
 
-	varglobal('colors', pieces.colors)
+	varglobal("colors", pieces.colors)
 
 	return pieces
 end
