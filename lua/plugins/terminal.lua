213f08a40446da3b0082bf20c82399666fc12278
matheus moreno <o.matheus.moreno@gmail.com>
2023-02-15 15:25:26 -0300

    added lua/plugins/terminal.lua

---
+++ lua/plugins/terminal.lua
@@ -0,0 +1,29 @@
+--------------- Terminal ---------------
+
+-- require(...)
+
+---------- Verification Step
+
+local modules = dependencies({ "toggleterm" })
+
+if not modules then
+	return
+end
+
+local module = modules("toggleterm")
+
+---------- Terminal Setup
+
+-- Setup
+
+---------- Terminal Functions
+
+-- Functions
+
+---------- Terminal Auto Commands
+
+-- Auto Commands
+
+---------- Terminal Mappings
+
+-- Mappings
