8857daa97c9208bb647733fb80c37a86858c008d
matheus moreno <o.matheus.moreno@gmail.com>
2023-03-06 22:28:34 -0300

    added some configs to terminal plugin

--- lua/plugins/terminal.lua
+++ lua/plugins/terminal.lua
@@ -15,14 +15,21 @@ local terminal = modules("toggleterm")
 ---------- Terminal Setup
 
 terminal.setup({
-	direction = "float",
+	direction = "horizontal",
+	open_mapping = "<a-t>t",
+	close_on_exit = true,
 	on_open = function(term)
-		vim.api.nvim_buf_set_name(term.bufnr, ("Terminal [%s]"):format(term.id))
+		local close = function()
+			if get("filetype") == "toggleterm" then
+				if eval["mode"]() == "n" then
+					execute("q")
+				end
+			end
+		end
 
-		setwindow("number", false, term.window)
-		setwindow("relativenumber", false, term.window)
+		nnoremap("<esc>", close, { silent = true, buffer = term.bufnr })
 
-		print(getwindow("number", term.window))
+		--vim.api.nvim_buf_set_name(term.bufnr, ("Terminal [%s]"):format(term.id))
 	end,
 	size = function(screen)
 		if screen.direction == "horizontal" then
@@ -56,8 +63,16 @@ terminal.setup({
 
 ---------- Terminal Auto Commands
 
--- Auto Commands
+augroup("Terminal")
+do
+	autocmd("Terminal", "BufEnter", "term://*", function()
+		if eval["mode"]() == "n" then
+			execute("normal! i")
+		end
+	end)
+end
 
 ---------- Terminal Mappings
 
--- Mappings
+-- nnoremap("<a-t>f", bundle(open, "float"), { silent = true })
+-- nnoremap("<a-t>t", bundle(open, "horizontal"), { silent = true })
