e3a904f64ff070846eae7a11e088813614e4fe92
gauloish <125079293+gauloish@users.noreply.github.com>
2024-02-16 13:38:35 -0300

    added new neovim configuration wit nvchad

---
+++ chadrc.lua
@@ -0,0 +1,79 @@
+local M = {}
+
+M.options = {
+    nvchad_branch = "v2.0",
+}
+
+M.ui = {
+    -- cmp themeing
+    cmp = {
+        icons = true,
+        lspkind_text = true,
+        style = "default", -- default/flat_light/flat_dark/atom/atom_colored
+        border_color = "grey_fg", -- only applicable for "default" style, use color names from base30 variables
+        selected_item_bg = "simple", -- colored / simple
+    },
+
+    telescope = { style = "borderless" }, -- borderless / bordered
+
+    ------------------------------- nvchad_ui modules -----------------------------
+    statusline = {
+        theme = "minimal", -- default/vscode/vscode_colored/minimal
+        -- default/round/block/arrow separators work only for default statusline theme
+        -- round and block will work for minimal theme only
+        separator_style = "round",
+        overriden_modules = function(modules)
+            table.insert(modules, 1, " ")
+        end,
+    },
+
+    -- lazyload it when there are 1+ buffers
+    tabufline = {
+        show_numbers = false,
+        enabled = true,
+        lazyload = false,
+        overriden_modules = nil,
+    },
+
+    -- nvdash (dashboard)
+    nvdash = {
+        load_on_startup = true,
+
+        header = {
+            "           ▄ ▄                   ",
+            "       ▄   ▄▄▄     ▄ ▄▄▄ ▄ ▄     ",
+            "       █ ▄ █▄█ ▄▄▄ █ █▄█ █ █     ",
+            "    ▄▄ █▄█▄▄▄█ █▄█▄█▄▄█▄▄█ █     ",
+            "  ▄ █▄▄█ ▄ ▄▄ ▄█ ▄▄▄▄▄▄▄▄▄▄▄▄▄▄  ",
+            "  █▄▄▄▄ ▄▄▄ █ ▄ ▄▄▄ ▄ ▄▄▄ ▄ ▄ █ ▄",
+            "▄ █ █▄█ █▄█ █ █ █▄█ █ █▄█ ▄▄▄ █ █",
+            "█▄█ ▄ █▄▄█▄▄█ █ ▄▄█ █ ▄ █ █▄█▄█ █",
+            "    █▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄█ █▄█▄▄▄█    ",
+        },
+
+        buttons = {
+            { "  Find File", "Spc f f", "Telescope find_files" },
+            { "󰈚  Recent Files", "Spc f o", "Telescope oldfiles" },
+            { "󰈭  Find Word", "Spc f w", "Telescope live_grep" },
+            { "  Bookmarks", "Spc m a", "Telescope marks" },
+            { "  Themes", "Spc t h", "Telescope themes" },
+            { "  Mappings", "Spc c h", "NvCheatsheet" },
+        },
+    },
+
+    cheatsheet = { theme = "grid" }, -- simple/grid
+
+    lsp = {
+        -- show function signatures i.e args as you type
+        signature = {
+            disabled = false,
+            silent = true, -- silences 'no signature help available' message from appearing
+        },
+    },
+}
+
+-- M.plugins = "custom.plugins"
+
+M.mappings = require "custom.mappings"
+
+return M
---
+++ init.lua
@@ -0,0 +1,16 @@
+local opt = vim.opt
+
+opt.clipboard = ""
+opt.cursorline = true
+
+-- Indenting
+opt.expandtab = true
+opt.shiftwidth = 4
+opt.smartindent = true
+opt.tabstop = 4
+opt.softtabstop = 4
+
+-- Numbers
+opt.number = true
+opt.relativenumber = true
+opt.numberwidth = 2
